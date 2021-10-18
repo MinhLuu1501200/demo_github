464196d022cc9e2baaf04ba2ad530590f91f229d
Tuan Anh Nguyen <67224723+seriousman1220@users.noreply.github.com>
2021-10-18 21:48:58 +0700

    Add files via upload

---
+++ baitapOOP_Csharp/Bai1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            DateTime dob2 = new DateTime(2001, 1, 1);
+            Programmer p3 = new Programmer(2, "Nguyen Tuan Anh", "Good job, amazing,", dob2);
+            Programmer p10 = new Programmer(10, "ABC", "good at JAVa", dob2);
+            p10.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
